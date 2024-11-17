c07f66710c8a3cae8f8bec80c7f4b92b31fc57cb
Harshitha Reddy <148534438+HarshithaReddy19@users.noreply.github.com>
2024-11-17 19:04:51 +0530

    Update bhanu's_message.py

--- visaa_prep/bhanu's_message.py
+++ visaa_prep/bhanu's_message.py
@@ -3,9 +3,12 @@ if len(x)==10 and x.isdigit():
     print("Correct")
 elif x.startswith('+') and ' ' in x:
     countrycode, num=x.split(' ',1)
-    if len(countrycode) in [2, 3] and countrycode[1:].isdigit() and len(num)==10 and num.isdigit():
-        print("Correct")
+    if len(countrycode)==3 and countrycode[1:].isdigit():
+        if len(num)==10 and num.isdigit():
+            print("Correct")
+        else:
+            print("Incorrect")
     else:
-        print("Incorrect")
+            print("Incorrect")
 else:
     print("Incorrect")
